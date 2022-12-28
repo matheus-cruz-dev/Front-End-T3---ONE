472da91380d2df3aefc1b39edf683f0ed2456088
matheus-cruz-dev <matheus.cruz.2004@hotmail.com>
2022-12-27 21:18:20 -0300

    end of js-poo course

--- js-poo/ContaCorrente.js
+++ js-poo/ContaCorrente.js
@@ -1,15 +1,36 @@
+import { Cliente } from "../js-poo/Cliente.js"
 export class ContaCorrente {
+  static numeroDeContas = 0
   agencia
-  cliente
-
+  _cliente
   // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
   _saldo = 0
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor
+    }
+  }
+
+  get cliente() {
+    return this._cliente
+  }
+
+  get saldo() {
+    return this._saldo
+  }
 
   sacar(valor) {
     if (this._saldo >= valor) {
       this._saldo -= valor
       return valor
     }
+  }
+
+  constructor(agencia, cliente) {
+    this.agencia = agencia
+    this.cliente = cliente
+    ContaCorrente.numeroDeContas += 1
   }
 
   depositar(valor) {
